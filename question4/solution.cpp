dd02a69dd3065bbbbfa4929b2442cc9c41404bd5
GeetikaNahak <geetikasaisravaninahak@gmail.com>
2024-11-13 19:57:31 +0530

    question 4

--- question4/solution.cpp
+++ question4/solution.cpp
@@ -1,45 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
+
+bool isValidMessage(const string &num) {
+    int evenSum = 0, oddSum = 0;
+
+    for (int i = 0; i < num.length(); i++) {
+        if (i % 2 == 0) {
+            evenSum += num[i] - '0';
+        } else {
+            oddSum += num[i] - '0';
+        }
+    }
+    return evenSum == oddSum;
+}
 
 int main() {
     int N;
-    cin >> N;  // Read the number of chocolate purchases
-    
-    vector<int> chocolates(N);
-    int maxID = 0;
-    
-    // Read the chocolate IDs and find the maximum ID
-    for (int i = 0; i < N; ++i) {
-        cin >> chocolates[i];
-        maxID = max(maxID, chocolates[i]);
+    cin >> N;
+    string num;
+    cin >> num;
+
+    if (isValidMessage(num)) {
+        cout << "message is valid" << endl;
+    } else {
+        cout << "message is not valid" << endl;
     }
-
-    // Create a frequency array to count occurrences of each chocolate ID
-    vector<int> freq(maxID + 1, 0);  // Using maxID + 1 because IDs are 1-based
-
-    // Count the frequency of each chocolate ID
-    for (int i = 0; i < N; ++i) {
-        freq[chocolates[i]]++;
-    }
-
-    // Find the maximum frequency
-    int maxFreq = 0;
-    for (int i = 0; i <= maxID; ++i) {
-        maxFreq = max(maxFreq, freq[i]);
-    }
-
-    // Count how many chocolates have the maximum frequency
-    int favoriteCount = 0;
-    for (int i = 0; i <= maxID; ++i) {
-        if (freq[i] == maxFreq) {
-            favoriteCount++;
-        }
-    }
-
-    // Output the result
-    cout << favoriteCount << endl;
 
     return 0;
 }
